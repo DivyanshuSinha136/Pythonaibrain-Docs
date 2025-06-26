0f4d8471d6191e55b0a355b3aa44f0c87fab8480
Divyanshu Sinha <divyanshu.sinha136@gmail.com>
2025-06-26 22:39:16 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,32 +1,22 @@
-# Project information
-project = 'pythonaibrain'
-author = 'Divyanshu Sinha'
-version = '1.0'
-release = '1.0.8'
+# docs/conf.py
+project = "pythonaibrain"
+author = "Divyanshu Sinha"
+version = "1.1.8"
+release = "1.1.8"
 
-# Extensions
+# Enable Sphinx extensions
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.todo',
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
-# Source and build directories
-source_suffix = '.rst'
-master_doc = 'index'
+# Support .rst and .md files
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
 
 # Theme
-html_theme = 'sphinx_rtd_theme'
-html_theme_options = {
-    'style': 'default',
-}
-
-# Static files
-html_static_path = ['_static']
-
-# Autodoc settings
-autodoc_default_options = {
-    'member-order': 'bysource',
-    'special-members': '__init__',
-    'undoc-members': True,
-}
+html_theme = "sphinx_rtd_theme"
